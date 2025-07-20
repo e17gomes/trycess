e75d3b907286dd7e6533538042748892981cc5c3
e17gomes <e17gomes@gmail.com>
2025-07-20 15:11:10 -0300

    chore(middleware): inline comments at  middleware for readbility improves

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,55 +1,42 @@
-import { NextRequest, type MiddlewareConfig ,NextResponse } from 'next/server'
- 
-const publicRoutes = [{
-    path:'/', whenAuthIsAllowed:'next'
-},{
-    path:'/login', whenAuthIsAllowed:'redirect'
-    },
-{
-    path:'/register', whenAuthIsAllowed:'redirect'
-    },] as const
+import { NextRequest, type MiddlewareConfig, NextResponse } from 'next/server'
+
+const publicRoutes = [
+    { path: '/', whenAuthIsAllowed: 'next' },
+    { path: '/login', whenAuthIsAllowed: 'redirect' },
+    { path: '/register', whenAuthIsAllowed: 'redirect' },
+] as const
 
 export function middleware(req: NextRequest) {
-    const currentPath = req.nextUrl.pathname;
+    const currentPath = req.nextUrl.pathname
     const publicRoute = publicRoutes.find(route => route.path === currentPath)
     const authToken = req.cookies.get('tkn')
-
+    // Usuário não autenticado e rota não pública: redireciona para login
     if (!authToken && !publicRoute) {
         const urlToRedirect = req.nextUrl.clone()
         urlToRedirect.pathname = '/login'
-
         return NextResponse.redirect(urlToRedirect)
     }
-    
+
+    // Usuário autenticado e rota não pública: passa sem problemas (como são dados mockados não seria necessário validação)
     if (authToken && !publicRoute) {
-        const urlToRedirect = req.nextUrl.clone()
-        urlToRedirect.pathname = '/login'
-
-        return NextResponse.redirect(urlToRedirect)
+        return NextResponse.next()
     }
 
-    if (authToken && publicRoute && publicRoute.whenAuthIsAllowed === 'redirect') {
+    // Usuário autenticado e rota pública que exige redirecionamento: redireciona para dashboard
+    if (authToken && publicRoute && publicRoute?.whenAuthIsAllowed === 'redirect') {
         const urlToRedirect = req.nextUrl.clone()
         urlToRedirect.pathname = '/dashboard'
-
         return NextResponse.redirect(urlToRedirect)
     }
 
+    // Caso padrão: segue normalmente
     return NextResponse.next()
 }
 
 export const config = {
     matcher: [
-      //Basicamente é a configuração em condições que o middleware não deve ser ativado
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
-     */
-    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
-  ],
+        '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
+    ],
 }
 
 export default middleware
